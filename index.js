c8dcbc039e18f19389a115f14887633cac31880f
kumaraditya2002 <adityakumar9006@gmail.com>
2022-06-16 13:25:39 +0530

    changes index.js

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ mongoose.connect(
 
 
 app.listen(process.env.PORT,()=>{
-    console.log(`Server is running at port:${PORT}`);
+    console.log(`Server is running at port:${process.env.PORT}`);
 });
